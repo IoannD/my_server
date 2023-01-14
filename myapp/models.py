05dacd7b9245bd6463ff7e31adbc88b2249fb664
IoannD <hdobrov@gmail.com>
2023-01-14 16:12:20 +0300

    save data in DB

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    user_name = models.CharField(max_length=50)
+    age = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.user_name
+
+    def was_user_adult(self):
+        return self.age >= 18
+    
--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+
+from .models import User
 # Create your views here.
 
 data_structure = [
@@ -25,7 +27,9 @@ def http_server(request):
 
         print(f'get_data["name"] = {get_data["name"]}')
         print()
-
+        new_user = User(user_name=get_data["name"], age=get_data["age"])
+        new_user.save()
+        print(f'new_user.id = {new_user.id}')
         data_structure.append(get_data)
 
         print(data_structure)
